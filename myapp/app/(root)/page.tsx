ac343288962440d8373d999fa74da61b1959c36f
Aman <amankumar105541@gmail.com>
2026-03-01 22:28:02 +0530

    ...

--- myapp/app/(root)/page.tsx
+++ myapp/app/(root)/page.tsx
@@ -1,11 +1,21 @@
 import {Button} from "@/components/ui/button";
-
+import MARKET_OVERVIEW_WIDGET_CONFIG from "@/lib/constants";
 
 const Home = () => {
+    const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
+
     return (
-        <div className="flex min-h-screen home-wrapper">
-           home
-        </div>
+        <section className="grid w-full gap-8 home-section">
+            <div className="md:col-span-1 xl:col-span-1">
+                <TradingViewWidget
+                    title="Market Overview"
+                    scriptUrl={`${scriptUrl}market-overview.js`}
+                    config={MARKET_OVERVIEW_WIDGET_CONFIG}
+                    className="custom-chart"
+                    height={600}
+                />
+            </div>
+        </section>
     )
 }
 export default Home
