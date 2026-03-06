7bd9fc0b2455d85f34c22b46fae76c8e58d68d44
Aman <amankumar105541@gmail.com>
2026-03-06 22:52:13 +0530

    started working on authentication

--- myapp/lib/constants.ts
+++ myapp/lib/constants.ts
@@ -38,7 +38,7 @@ export const CONDITION_OPTIONS = [
 
 // TradingView Charts
 export const MARKET_OVERVIEW_WIDGET_CONFIG = {
-   g // dark mode
+    colorTheme: 'dark', // dark mode
     dateRange: '12M', // last 12 months
     locale: 'en', // language
     largeChartUrl: '', // link to a large chart if needed
